900aaa70a24a723504fda5f455a49c6dd49e7ea3
Andrew <thum14252@gmail.com>
2024-05-07 17:43:23 +0630

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
+import { useState } from 'react';
+import Header from './Components/Header';
 import './App.css'
 
 function App() {
  
 
   return (
-    <>
-     
-    </>
+    <div className=''>
+     <Header/>
+    </div>
   )
 }
 
